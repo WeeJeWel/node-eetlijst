762e1b56337170bfb8211dcf603b773195c6f2c6
Emile Nijssen <emile@athom.nl>
2016-11-25 16:18:24 +0100

    version bump

--- examples/demo.js
+++ examples/demo.js
@@ -6,22 +6,16 @@ let username = process.argv[2];
 let password = process.argv[3];
 
 let eetlijst = new Eetlijst( username, password );
-	/*
 	eetlijst
 		.getSummary()
 		.then( summary => {
 			console.log('summary', JSON.stringify(summary, false, 4));
-		})
-		.catch( err => {
-			console.error('err', err);
-		})
-	*/
 
-
-	eetlijst
-		.setUserState('Bas', 'eat')
-		.then( success => {
-			console.log('success', success);
+			return eetlijst
+				.setUserState('Robert', 'cook')
+				.then( success => {
+					console.log('success', success);
+				})
 		})
 		.catch( err => {
 			console.error('err', err);
